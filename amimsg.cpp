5b8219b8bee9707b9adb3e523e5019fd438f15ee
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-17 20:28:45 +0300

    src: building the message :D

--- amimsg.cpp
+++ amimsg.cpp
@@ -13,3 +13,32 @@ AmiMsg::AmiMsg()
 }
 
 
+
+AmiMsg& AmiMsg::addAction(const QString &act)
+{
+    m_msgForAmi.append(act);
+    m_msgForAmi.append("\n");
+    return *this;
+}
+
+
+AmiMsg& AmiMsg::addEvent(const QString &ev)
+{
+    m_msgForAmi.append(ev);
+    m_msgForAmi.append("\n");
+    return *this;
+}
+
+
+AmiMsg& AmiMsg::addResponse(const QString &resp)
+{
+    m_msgForAmi.append(resp);
+    m_msgForAmi.append("\n");
+    return *this;
+}
+
+
+QString AmiMsg::build()
+{
+    return m_msgForAmi;
+}
--- amimsg.h
+++ amimsg.h
@@ -1,18 +1,36 @@
 #ifndef AMIMSG_H
 #define AMIMSG_H
+#include <QString>
 
 class AmiClient;
 
 class AmiMsg
 {
+// we care for these one for now
+    enum MsgType
+    {
+        EVENT,
+        ACTION,
+        RESPONSE
+    };
 
 public:
     explicit AmiMsg();
     virtual ~AmiMsg();
 
+    AmiMsg& addEvent(const QString& ev);
+    AmiMsg& addAction(const QString& act);
+    AmiMsg& addResponse(const QString& resp);
+
+    QString build(void);
+
 private:
     // on call, call my function //
+    QString     m_msgForAmi;
+
     AmiClient* m_callee; // who called me? //
+
+
 
 };
 
